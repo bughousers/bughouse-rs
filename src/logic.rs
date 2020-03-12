e622efd7a1ec7ec6e6625f0afdee31dc437c408d
cherrytoska <budanaz.yakup@gmail.com>
2020-03-12 03:24:02 +0100

    movement for pawns

--- src/logic.rs
+++ src/logic.rs
@@ -17,8 +17,14 @@ impl ChessLogic {
         self.chess_board1.print_board();
     }
 
-    pub fn print_w_legal(&self, x: Vec<(usize,usize)>){
-
+    pub fn print_w_legal(&mut self,board1:bool,locs: &Vec<(usize,usize)>){
+        for &(i,j) in locs.iter() {
+            self.chess_board1.board[i][j] = Piece::L;
+        }
+        self.chess_board1.print_board();
+        for &(i,j) in locs.iter() {
+            self.chess_board1.board[i][j] = Piece::E;
+        }
     }
 
     pub fn new() -> ChessLogic {
@@ -29,7 +35,7 @@ impl ChessLogic {
             unmoved_white_pawns: [(0,true); 8],
         }
     }
-    pub fn get_legal_moves(&self,old_i:usize, old_j:usize)
+    pub fn get_legal_moves(&self,board1:bool, old_i:usize, old_j:usize)
     -> Vec<(usize,usize)>
     {
         match self.chess_board1.board[old_i][old_j] {
@@ -37,19 +43,104 @@ impl ChessLogic {
                 match self.unmoved_white_pawns[old_j] {
                     (0,true) =>  {
                         let mut vec = Vec::new();
-                        vec.push((cmp::max(old_i-1,0),old_j));
-                        println!("legal to go to {}, {}",old_i-1,old_j);
-                        vec.push((cmp::max(old_i-2,0),old_j));
-                        println!("legal to go to {}, {}",old_i-2,old_j);
+                        if self.is_empty(board1,old_i-1,old_j) {
+                            vec.push((old_i-1,old_j));
+                            if self.is_empty(board1,old_i-2,old_j) {
+                                vec.push((old_i-2,old_j));
+                            }
+                        }
+                        if old_j > 0 && self.is_enemy(board1,true,old_i-1,old_j-1) {
+                            vec.push((old_i-1,old_j-1));
+                        }
+                        if old_j < 7 && self.is_enemy(board1,true,old_i-1,old_j+1) {
+                            vec.push((old_i-1,old_j+1));
+                        }
                         vec
                     },
                     (_,_) => {
                         let mut vec = Vec::new();
-                        vec.push((cmp::max(old)))
+                        if self.is_empty(board1,old_i-1,old_j) {
+                            vec.push((old_i-1,old_j));
+                        }
+                        if old_j > 0 && self.is_enemy(board1,true,old_i-1,old_j-1) {
+                            vec.push((old_i-1,old_j-1));
+                        }
+                        if old_j < 7 && self.is_enemy(board1,true,old_i-1,old_j+1) {
+                            vec.push((old_i-1,old_j+1));
+                        }
+                        vec
+                    },
+                }
+            },
+            Piece::p => {
+                match self.unmoved_black_pawns[old_j] {
+                    (0,true) =>  {
+                        let mut vec = Vec::new();
+                        if self.is_empty(board1,old_i+1,old_j) {
+                            vec.push((old_i+1,old_j));
+                            if self.is_empty(board1,old_i+2,old_j) {
+                                vec.push((old_i+2,old_j));
+                            }
+                        }
+                       
+                        if old_j > 0 && self.is_enemy(board1,false,old_i+1,old_j-1) {
+                            vec.push((old_i+1,old_j+1));
+                        }
+                        if old_j < 7 && self.is_enemy(board1,false,old_i+1,old_j+1) {
+                            vec.push((old_i-1,old_j+1));
+                        }
+                        vec
+                    },
+                    (_,_) => {
+                        let mut vec = Vec::new();
+                        if self.is_empty(board1,old_i+1,old_j) {
+                            vec.push((old_i+1,old_j));
+                        }
+                        if old_j > 0 && self.is_enemy(board1,false,old_i+1,old_j-1) {
+                            vec.push((old_i+1,old_j-1));
+                        }
+                        if old_j < 7 && self.is_enemy(board1,false,old_i+1,old_j+1) {
+                            vec.push((old_i+1,old_j+1));
+                        }
+                        vec
                     },
                 }
             },
             _ => Vec::new(),
         }
+    }
+
+    fn is_empty(&self, board1:bool, i:usize, j:usize) -> bool {
+        match self.chess_board1.board[i][j] {
+            Piece::E => true, 
+            _ => false,
+        }
+    }
+
+    fn is_enemy(&self, board1:bool,white:bool, i:usize, j:usize) -> bool {
+        match self.chess_board1.board[i][j] {
+            Piece::E => false,
+            Piece::L => false,
+            Piece::P => false,
+            Piece::R => false,
+            Piece::N => false,
+            Piece::B => false,
+            Piece::Q => false,
+            Piece::K => false,
+            _ => true,
+
+        }
+    }
+
+    fn vertical_mov(&self, i:usize, j:usize){
+        
+    }
+
+    fn horizontal_mov(&self, i:usize, j:usize){
+
+    }
+
+    fn cross_mov(&self, i:usize, j:usize){
+
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ fn main() {
     //chess_board.move_piece(1,1,3,1);
     //chess_board.print_board();
     let mut chess_logic = ChessLogic::new();
-    chess_logic.print();
-    chess_logic.get_legal_moves(6,1);
+    //let legals = chess_logic.get_legal_moves(true,6,7);
+    //chess_logic.print_w_legal(true,&legals);
+
+    chess_logic.print_w_legal(true,&chess_logic.get_legal_moves(true,1,0));
 }
